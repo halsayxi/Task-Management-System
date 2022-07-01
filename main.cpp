ad3a4a1d7d30931f9acba7cfe4e8ec84a6904400
halsayxi <7ffae611db724a1aa5f588accd6fe32d@example.com>
2022-07-02 00:01:15 +0800

    新建文件 main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//  PPCA日程管理_模块搭建
+//  created by halsayxi
+
+#include <iostream>
+#include "user.h"
+using namespace std;
+
+vector<user> users;//储存所有用户的容器
+
+//多线程处理
+int main()
+{
+    std::cout << "Hello, World!\n";
+    return 0;
+}
